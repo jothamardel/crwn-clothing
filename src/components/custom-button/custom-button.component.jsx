7c9cd174e02a502f91464d20aae2575beb5230de
Bee <ardelmbiplang@gmail.com>
2020-03-23 15:53:05 +0100

    Updated sign in with google stlye

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.styles.scss';
 
 
 const CustomButton = ({ children, isGoogleSignedIn, inverted, ...otherProps }) => (
-    <button className={`${ inverted ? 'inverted' : ''} custom-button`} { ...otherProps }>
+    <button className={`${ inverted ? 'inverted' : isGoogleSignedIn ? 'google-sign-in': ''  } custom-button`} { ...otherProps }>
         { children }
     </button>
 );
